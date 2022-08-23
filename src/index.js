23ecf9313127a786f183b8963d7a07a9b0b43a87
Nkasi-e <nkasijnr@gmail.com>
2022-08-23 19:21:58 +0100

    created and .env file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 //Loading the express
+require("dotenv").config();
 const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./router/user");
@@ -6,7 +7,7 @@ const taskRouter = require("./router/task");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 //Middleware
 
